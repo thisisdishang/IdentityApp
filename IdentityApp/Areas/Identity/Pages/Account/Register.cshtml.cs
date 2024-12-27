6797c7336e9d0a54ce4cacdc29ac9eb1cbc782aa
thisisdishang <dishangkumarrana@gmail.com>
2024-12-27 19:18:58 +0530

    Update input model and complete register page

--- IdentityApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ IdentityApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -71,6 +71,16 @@ namespace IdentityApp.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            [Required]
+            [StringLength(255,ErrorMessage ="Max 255 character are allowed")]
+            [Display(Name = "FirstName")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [StringLength(255, ErrorMessage = "Max 255 character are allowed")]
+            [Display(Name = "LastName")]
+            public string LastName { get; set; }
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
@@ -114,6 +124,9 @@ namespace IdentityApp.Areas.Identity.Pages.Account
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
+
+                user.FirstName = Input.FirstName;
+                user.LastName = Input.LastName; 
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
--- IdentityApp/Program.cs
+++ IdentityApp/Program.cs
@@ -24,6 +24,8 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.MapRazorPages();
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
